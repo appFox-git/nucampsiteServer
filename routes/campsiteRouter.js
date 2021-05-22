9f3fa9a6b9098a53fad99460dce33c3a9a26b373
Lee Fox <leefox78@live.com>
2021-05-22 11:50:16 -0500

    Added Mongoose Schemas and Models for Partners and Promotions. Revised partnerRouter and promotionRouter

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const { NetworkAuthenticationRequire } = require('http-errors');
 const Campsite = require('../models/campsite');
 
 const campsiteRouter = express.Router();
